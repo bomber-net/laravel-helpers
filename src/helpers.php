4fbc75240915874dc091594469c0243de2637a48
bomber-net <an-rtk@mail.ru>
2022-11-14 21:53:46 +0300

    initial commit

---
+++ src/helpers.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!function_exists ('str_normalize'))
+	{
+		function str_normalize (string $str):float|bool|int|string|null
+			{
+				return match (strtolower ($str))
+					{
+					'null'=>null,
+					'true'=>true,
+					'false'=>false,
+					default=>is_numeric ($str)?(is_intnum ($str)?(int)$str:(float)$str):$str,
+					};
+			}
+	}
+
+if (!function_exists ('is_intnum'))
+	{
+		function is_intnum (mixed $value):bool
+			{
+				$type=get_debug_type ($value);
+				if ($type==='int') return true;
+				$int=(int)$value;
+				settype ($int,$type);
+				return $int===$value;
+			}
+	}
