51272585da9a63e043bae00a818c4bd0e95e4743
bomber-net <an-rtk@mail.ru>
2022-11-15 18:48:49 +0300

    add hexbin function

--- src/helpers.php
+++ src/helpers.php
@@ -38,3 +38,25 @@ if (!function_exists ('sign'))
 					};
 			}
 	}
+
+if (!function_exists ('hexbin'))
+	{
+		function hexbin (string $hex):?string
+			{
+				static $convert=
+					[
+						'0'=>'0000','1'=>'0001','2'=>'0010','3'=>'0011',
+						'4'=>'0100','5'=>'0101','6'=>'0110','7'=>'0111',
+						'8'=>'1000','9'=>'1001','a'=>'1010','b'=>'1011',
+						'c'=>'1100','d'=>'1101','e'=>'1110','f'=>'1111',
+					];
+				$hex=strtolower ($hex);
+				$bin='';
+				foreach (str_split ($hex) as $char)
+					{
+						if (!($char=$convert[$char]??null)) return null;
+						$bin.=$char;
+					}
+				return $bin;
+			}
+	}
